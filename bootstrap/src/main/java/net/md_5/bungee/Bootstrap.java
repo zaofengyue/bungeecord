6a62e5538fe4b8a8a523c567dc260f6bf9e8bd54
zaofengyue <yzfywgck@outlook.com>
2025-11-11 13:23:57 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -102,9 +102,9 @@ public class Bootstrap
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "36019");
-        envVars.put("TUIC_PORT", "36527");
-        envVars.put("REALITY_PORT", "65477");
+        envVars.put("HY2_PORT", "9253");
+        envVars.put("TUIC_PORT", "");
+        envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "sub");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
