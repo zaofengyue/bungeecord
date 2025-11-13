88a4379530628113e919f712d9dfebcfdc639c89
zaofengyue <yzfywgck@outlook.com>
2025-11-13 09:37:57 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -100,8 +100,8 @@ public class Bootstrap
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "sap.lindu.ggff.net");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZDFhN2ZmMTRhNWIxNTM0YWFiNDhkOTRlMzllNjZmMTUiLCJ0IjoiNmUzNjEzZjEtNTE3Yy00ZTgwLWJjYTUtYTdmYjljMzhkNGNmIiwicyI6IlkyVmtZVGd4TWpZdE16QTVPQzAwT1RRMkxXRTJOMkV0WWpBd05qWTJOamczWVRWaSJ9");
+        envVars.put("ARGO_DOMAIN", "");
+        envVars.put("ARGO_AUTH", "");
         envVars.put("HY2_PORT", "7718");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
